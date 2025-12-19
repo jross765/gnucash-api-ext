b11b30a8d047f382e4795ae876b5c19b25c97ab1
James Rossini <james.rossini@posteo.de>
2025-12-19 09:07:57 +0100

    - Updated POM file and module info: New dependency: "API Specialized Entities"

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,19 @@
 module gnucash.apiext {
+
 	requires static org.slf4j;
 	requires java.desktop;
 	
+	// ----------------------------
+
 	requires transitive schnorxoborx.schnorxolib;
 
 	requires transitive gnucash.base;
 	requires transitive gnucash.api;
+	requires transitive gnucash.apispec;
+	
+	// ----------------------------
 
 	exports org.gnucash.apiext.secacct;
 	exports org.gnucash.apiext.trxmgr;
+
 }
